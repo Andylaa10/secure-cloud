1d43842afdec8dbab307247b29a95de15d84a9fa
Andy lam <91528350+Andylaa10@users.noreply.github.com>
2024-11-21 21:56:22 +0100

    initial setup

---
+++ secure-cloud-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex w-full min-h-screen bg-rose-900 justify-center items-center">
+        <h1 className="text-3xl font-bold underline">asd</h1>
+    </div>
+  )
+}
+
+export default App
